3de23e75ab044339149cf5ce65db38aab5f5eeb6
zeyad ahmed <zeyadmokled@hotmail.com>
2022-12-24 19:51:28 +0200

    validation to queue and made class bullet

--- src/dev/tahkeer/tadmer/controller/Game.java
+++ src/dev/tahkeer/tadmer/controller/Game.java
@@ -120,7 +120,7 @@ public class Game implements World {
                 } }
                 if(rightqueue.size()==0){
                      if (obstacle.getY() >= clown.getY() - 10
-                        && obstacle.getY() <= clown.getY() + 20
+                        && obstacle.getY() <= clown.getY() + 5
                         && obstacle.getX() >= clown.getX() - 10 + 160
                         && obstacle.getX() <= clown.getX() + clown.getWidth()
                         && obstacle.getX() + obstacle.getWidth() <= clown.getX() + 80 + 160
@@ -137,8 +137,9 @@ public class Game implements World {
                 }}
                 if(rightqueue.size()>0) {
                     rightcheck = new Point(rightqueue.get(rightqueue.size() - 1).getX(), rightqueue.get(rightqueue.size() - 1).getY());
-                    if (rightcheck.distance(obstacle.getX(), obstacle.getY()) >= 5&&rightcheck.distance(obstacle.getX(), obstacle.getY()) <20) {
+                    if (rightcheck.distance(obstacle.getX(), obstacle.getY()) >= 5&&rightcheck.distance(obstacle.getX(), obstacle.getY()) <20&&obstacle.getX()<600&& rightqueue.size()<22) {
                         movable.remove(obstacle);
+                        System.out.println(obstacle.getY()+"da el y .. dah el x: "+obstacle.getX());
                         obstacle.setY(obstacle.getY() - 5);
                         controllable.add(obstacle);
                         rightqueue.add(obstacle);
@@ -147,7 +148,7 @@ public class Game implements World {
                 }
                 if(leftqueue.size()>0) {
                     leftcheck = new Point(leftqueue.get(leftqueue.size() - 1).getX(), leftqueue.get(leftqueue.size() - 1).getY());
-                    if (leftcheck.distance(obstacle.getX(), obstacle.getY()) >= 5&&leftcheck.distance(obstacle.getX(), obstacle.getY()) <20) {
+                    if (leftcheck.distance(obstacle.getX(), obstacle.getY()) >= 5&&leftcheck.distance(obstacle.getX(), obstacle.getY()) <20&&obstacle.getY()>13&& leftqueue.size()<22) {
                         movable.remove(obstacle);
                         obstacle.setY(obstacle.getY() - 5);
                         controllable.add(obstacle);
