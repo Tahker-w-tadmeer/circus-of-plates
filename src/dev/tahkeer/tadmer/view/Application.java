14a32027049f4aadf9f188a64dba8e023f305766
Fadi Sarwat <fadisarwat@yahoo.com>
2022-12-29 05:16:34 +0200

    Pause and resume buttons

--- src/dev/tahkeer/tadmer/view/Application.java
+++ src/dev/tahkeer/tadmer/view/Application.java
@@ -3,27 +3,27 @@ package dev.tahkeer.tadmer.view;
 import dev.tahkeer.tadmer.controller.Game;
 import eg.edu.alexu.csd.oop.game.GameEngine;
 
-public class Application {
-    public static void main(String[] args) {
-        GameEngine.GameController controller = GameEngine.start("Circus Of Plates", Game.getInstance());
+import javax.swing.*;
 
-//        Score.getInstance().addListener(new ScoreEventListener() {
-//            @Override
-//            public void added(int oldScore, int score) {
-//                controller.pause();
-//                try {
-//                    Thread.sleep(10);
-//                } catch (InterruptedException e) {
-//                    throw new RuntimeException(e);
-//                }finally {
-//                    controller.resume();
-//                }
-//            }
-//
-//            @Override
-//            public void removed(int oldScore, int score) {
-//
-//            }
-//        });
+public class Application {
+
+    public static void main(String[] args) {
+
+        JMenuBar menuBar = new JMenuBar();
+        JMenu menu = new JMenu("Game");
+
+        GameEngine.GameController controller = GameEngine.start("Circus Of Plates", Game.getInstance(), menuBar);
+
+        JMenuItem pause = new JMenuItem("Pause");
+        pause.addActionListener(e -> controller.pause());
+        pause.setAccelerator(KeyStroke.getKeyStroke('p'));
+
+        JMenuItem resume = new JMenuItem("Resume");
+        resume.addActionListener(e -> controller.resume());
+        resume.setAccelerator(KeyStroke.getKeyStroke('r'));
+
+        menu.add(pause);
+        menu.add(resume);
+        menuBar.add(menu);
     }
 }
